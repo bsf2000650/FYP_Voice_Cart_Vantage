071c915ec5d7d130a3954db37ad61866a7ed2b61
my name <myemailid@example.com>
2023-11-03 21:32:37 -0700

    Changed url in frontend for backend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,4 +1,4 @@
-export const server = "http://localhost:8000/api/v2";
-export const backend = "http://localhost:8000/";
+export const server = "https://e-shop1-beryl.vercel.app/api/v2";
+export const backend = "https://e-shop1-beryl.vercel.app/";
 
 
