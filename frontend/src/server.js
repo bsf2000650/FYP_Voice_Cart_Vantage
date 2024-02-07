9a2d096b2c5bc7fb5b78f4d9284b56e36d62999e
Muhammad Asad <114976729+bsf2000650@users.noreply.github.com>
2024-02-07 01:20:02 -0800

    Update server.js

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
-// export const server = "https://e-shop1-beryl.vercel.app/api/v2";
-// export const backend = "https://e-shop1-beryl.vercel.app/";
-export const server = "http://localhost:8000/api/v2";
-export const backend = "http://localhost:8000/";
+export const server = "https://35-ecommerce-mern-project.vercel.app/api/v2";
+export const backend = "https://35-ecommerce-mern-project.vercel.app/";
+// export const server = "http://localhost:8000/api/v2";
+// export const backend = "http://localhost:8000/";
 
 
