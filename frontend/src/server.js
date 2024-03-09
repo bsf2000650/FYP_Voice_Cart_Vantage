dd685aec6c62f1811bec028a5be28ec1b5cd3f30
asad <bsf2000650@ue.edu.pk>
2024-03-09 04:39:19 -0800

    Added frontend backend to frontend

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,6 +1,6 @@
-// export const server = "https://35-ecommerce-mern-project.vercel.app/api/v2";
-// export const backend = "https://35-ecommerce-mern-project.vercel.app/";
-export const server = "http://localhost:8000/api/v2";
-export const backend = "http://localhost:8000/";
+export const server = "https://fyp-voice-cart-vantage-og4g.vercel.app/api/v2";
+export const backend = "https://fyp-voice-cart-vantage-og4g.vercel.app/";
+// export const server = "http://localhost:8000/api/v2";
+// export const backend = "http://localhost:8000/";
 
 
