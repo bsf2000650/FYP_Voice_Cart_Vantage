528eb82a5c533fb281efa4f31ae16ee2a0288b89
asad <bsf2000650@ue.edu.pk>
2024-05-08 20:55:07 -0700

    Corrected front end urls

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,5 +1,5 @@
 export const server = "https://fyp-voice-cart-vantage-og4g.vercel.app/api/v2";
-export const backend = "https://fyp-voice-cart-vantage-og4g.vercel.app/";
+export const backend = "https://fyp-voice-cart-vantage-og4g.vercel.app/";    
 // export const server = "http://localhost:8000/api/v2";
 // export const backend = "http://localhost:8000/";
 
